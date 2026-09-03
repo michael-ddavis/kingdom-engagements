b7c1ceec217f728aac60efcb221532caa252bce8
michael-davis <47865749+michael-ddavis@users.noreply.github.com>
2026-09-03 16:22:55 +0000

    Route DWC formation through simplified group-aware views

--- src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
+++ src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { InvitationsComponent } from './pages/invitations.component';
 import { AssignmentListComponent } from './pages/assignment-list.component';
 import { AssignmentWorkspaceComponent } from './pages/assignment-workspace.component';
-import { DwcFormationComponent } from './pages/dwc-formation.component';
 import { DwcFormationHomeComponent } from './pages/dwc-formation-home.component';
-import { DwcMyGroupComponent } from './pages/dwc-my-group.component';
+import { DwcFormationToolsComponent } from './pages/dwc-formation-tools.component';
+import { DwcMyGroupContextComponent } from './pages/dwc-my-group-context.component';
 import { OrganizationLandingComponent } from './pages/organization-landing.component';
 import { OrganizationProgramsComponent } from './pages/organization-programs.component';
 
@@ -19,8 +19,8 @@ export const appConfig: ApplicationConfig = {
       { path: 'assignments', component: AssignmentListComponent },
       { path: 'assignments/:id', component: AssignmentWorkspaceComponent },
       { path: 'organization/dwc/formation', component: DwcFormationHomeComponent },
-      { path: 'organization/dwc/formation/tools', component: DwcFormationComponent },
-      { path: 'organization/dwc/my-group', component: DwcMyGroupComponent },
+      { path: 'organization/dwc/formation/tools', component: DwcFormationToolsComponent },
+      { path: 'organization/dwc/my-group', component: DwcMyGroupContextComponent },
       { path: 'organization/:org', component: OrganizationProgramsComponent },
       { path: '**', redirectTo: '' },
     ]),

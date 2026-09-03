350c8aa3a86062d59e259801c20c5688b8a616da
michael-davis <47865749+michael-ddavis@users.noreply.github.com>
2026-09-03 16:04:45 +0000

    refactor: separate simple DWC formation home from ministry tools

--- src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
+++ src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { InvitationsComponent } from './pages/invitations.component';
 import { AssignmentListComponent } from './pages/assignment-list.component';
 import { AssignmentWorkspaceComponent } from './pages/assignment-workspace.component';
 import { DwcFormationComponent } from './pages/dwc-formation.component';
+import { DwcFormationHomeComponent } from './pages/dwc-formation-home.component';
 import { DwcMyGroupComponent } from './pages/dwc-my-group.component';
 import { OrganizationLandingComponent } from './pages/organization-landing.component';
 import { OrganizationProgramsComponent } from './pages/organization-programs.component';
@@ -17,7 +18,8 @@ export const appConfig: ApplicationConfig = {
       { path: 'invitations', component: InvitationsComponent },
       { path: 'assignments', component: AssignmentListComponent },
       { path: 'assignments/:id', component: AssignmentWorkspaceComponent },
-      { path: 'organization/dwc/formation', component: DwcFormationComponent },
+      { path: 'organization/dwc/formation', component: DwcFormationHomeComponent },
+      { path: 'organization/dwc/formation/tools', component: DwcFormationComponent },
       { path: 'organization/dwc/my-group', component: DwcMyGroupComponent },
       { path: 'organization/:org', component: OrganizationProgramsComponent },
       { path: '**', redirectTo: '' },

18f5407073b2d4f2b36945a468d34b08d5306380
michael-davis <47865749+michael-ddavis@users.noreply.github.com>
2026-09-08 15:01:10 +0000

    feat: guard Engagements routes by demo role

--- src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
+++ src/KingdomEngagements.Web/ClientApp/src/app/app.config.ts
@@ -1,5 +1,5 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, inject, provideAppInitializer } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { InvitationsComponent } from './pages/invitations.component';
 import { AssignmentListComponent } from './pages/assignment-list.component';
@@ -15,19 +15,25 @@ import { DwcFormationToolsComponent } from './pages/dwc-formation-tools.componen
 import { DwcMyGroupContextComponent } from './pages/dwc-my-group-context.component';
 import { OrganizationLandingComponent } from './pages/organization-landing.component';
 import { OrganizationProgramsComponent } from './pages/organization-programs.component';
+import {
+  EngagementDemoRoleService,
+  engagementBookingGuard,
+} from './core/engagement-demo-role.service';
+import { engagementDemoRoleInterceptor } from './core/engagement-demo-role.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([engagementDemoRoleInterceptor])),
+    provideAppInitializer(() => inject(EngagementDemoRoleService).mountSwitcher()),
     provideRouter([
       { path: '', component: OrganizationLandingComponent, pathMatch: 'full' },
-      { path: 'invitations', component: InvitationsComponent },
+      { path: 'invitations', component: InvitationsComponent, canActivate: [engagementBookingGuard] },
       { path: 'assignments', component: AssignmentListComponent },
       { path: 'assignments/:id', component: AssignmentWorkspaceComponent },
-      { path: 'organization/ctg', component: CtgEngagementsHomeComponent },
-      { path: 'organization/ctg/bookings', component: CtgBookingDeskComponent },
-      { path: 'organization/ctg/start-invitation', component: CtgStartInvitationComponent },
-      { path: 'organization/ctg/programs', component: CtgProgramsComponent },
+      { path: 'organization/ctg', component: CtgEngagementsHomeComponent, canActivate: [engagementBookingGuard] },
+      { path: 'organization/ctg/bookings', component: CtgBookingDeskComponent, canActivate: [engagementBookingGuard] },
+      { path: 'organization/ctg/start-invitation', component: CtgStartInvitationComponent, canActivate: [engagementBookingGuard] },
+      { path: 'organization/ctg/programs', component: CtgProgramsComponent, canActivate: [engagementBookingGuard] },
       { path: 'register/:eventId', component: CtgEventRegistrationComponent },
       { path: 'join-the-12', component: CtgPower12ApplicationComponent },
       { path: 'organization/dwc/formation', component: DwcFormationHomeContextComponent },
